a6ebee616fd4e505c3856674084dbbce311f6738
Imam26 <i.m.a.m.sabitov@mail.ru>
2018-10-30 15:41:25 +0600

    first-commit

---
+++ SecondTask/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentList = new List<Student>()
+            {
+                new Student("Студент1", "группа1", 7.8, 30000, Sex.man, FormOfStudy.extramural),
+                new Student("Студент2", "группа1", 7.9, 9000, Sex.man, FormOfStudy.extramural),
+                new Student("Студент3", "группа1", 6.7, 12000, Sex.woman, FormOfStudy.extramural),
+                new Student("Студент4", "группа2", 5.8, 18800, Sex.man, FormOfStudy.fullTime),
+                new Student("Студент5", "группа2", 10, 6000, Sex.woman, FormOfStudy.fullTime),
+                new Student("Студент6", "группа2", 8.1, 11500, Sex.man, FormOfStudy.fullTime),
+                new Student("Студент7", "группа1", 8.8, 9200, Sex.woman, FormOfStudy.extramural),
+            };
+
+            int minSalary = 10000;
+
+            Console.WriteLine("Размер двух минимальных зарплат: {0}", minSalary);
+            Console.WriteLine("**********Порядок получения общежития: ");
+
+            var firstQueue = studentList.Where(x => x.IncomePerFamilyMember < minSalary).OrderBy(x=>x.IncomePerFamilyMember);
+            var secondQueue = studentList.Where(x => x.IncomePerFamilyMember > minSalary).OrderByDescending(x => x.AvarageScore);
+
+            int number = 1;
+
+            foreach(Student student in firstQueue.Concat(secondQueue))
+            {
+                Console.WriteLine("------------{0}-------------", number++);
+                student.Show();
+            }
+        }
+    }
+
+    struct Student
+    {
+        public string FullName { get; set; }
+        public string Group { get; set; }
+        public double AvarageScore { get; set; }
+        public int IncomePerFamilyMember { get; set; }
+        public Sex StudentSex { get; set; }
+        public FormOfStudy StudyForm { get; set; }
+
+        public Student(string fullName, string group, double avarageScore, int income, Sex sex, FormOfStudy form)
+        {
+            FullName = fullName;
+            Group = group;
+            AvarageScore = avarageScore;
+            IncomePerFamilyMember = income;
+            StudentSex = sex;
+            StudyForm = form;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Полное имя студента: {0}", FullName);
+            Console.WriteLine("Средний доход на члена семьи: {0}", IncomePerFamilyMember);
+            Console.WriteLine("Средний балл: {0}", AvarageScore);
+        }
+    }
+
+    enum Sex { man, woman };
+
+    enum FormOfStudy { fullTime, extramural }
+
+}
---
+++ StructureAndEnum/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = 
+            {
+                new Employee("ЕМенеджеров", "Менеджер","менеджер",new DateTime(2012, 10, 21), 135000),
+                new Employee("АМенеджеров", "Менеджер", "менеджер", new DateTime(2011, 09,22), 90000),
+                new Employee("БМенеджеров","Менеджер", "менеджер", new DateTime(2014, 09,22), 60000),
+                new Employee("АМенеджеров","Менеджер", "менеджер", new DateTime(2016, 09,22), 1400000),
+                new Employee("СМенеджеров", "Менеджер", "менеджер", new DateTime(2013, 09,22), 100000),
+                new Employee("ВКлерков", "Клерк1", "клерк", new DateTime(2015, 09,22), 80000),
+                new Employee("СКлерков", "Клерк2", "клерк", new DateTime(2009, 09,22), 150000),
+                new Employee("ЕКлерков", "Клерк3", "клерк", new DateTime(2010, 09,22), 60000),
+                new Employee("АКлерков", "Клерк4", "клерк", new DateTime(2011, 09,22), 230000),
+                new Employee("Босов", "Бос", "босс", new DateTime(2013, 09,22), 500000),
+            };
+
+            Console.WriteLine("************Задача 1*************");
+            Console.WriteLine("Полная информация о сотрудниках: ");
+
+            foreach(Employee tempEmployee in employees)
+            {
+                Console.WriteLine("----------------------");
+                tempEmployee.Show();
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            double avarageSalary = employees.Where(x => x.Position == "клерк" ? true : false).Average(x => x.Salary);
+
+            Console.WriteLine("************Задача 2*************");
+            Console.WriteLine("Средняя заработная плата клерков: {0} тенге", avarageSalary);
+            Console.WriteLine("Список менеджеров: ");
+
+            var selectedManager = from employee in employees
+                       where (employee.Position == "менеджер")&&
+                       (employee.Salary>avarageSalary)
+                       orderby employee.Surname
+                       select employee;
+
+            foreach(Employee tempEmployee in selectedManager)
+            {
+                Console.WriteLine("----------------------");
+                tempEmployee.Show();
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("************Задача 3*************");
+            Console.WriteLine("Дата принятия босса на работу: 2013/09/22");
+            Console.WriteLine("Список сотрудников: ");
+
+            var selectedEmployees = from employee in employees
+                                     where employee.DateOfEmployment > new DateTime(2013, 09, 22)
+                                     orderby employee.Surname
+                                     select employee;
+
+            foreach(Employee tempEmployee in selectedEmployees)
+            {
+                Console.WriteLine("----------------------");
+                tempEmployee.Show();
+            }
+        }
+    }
+
+    struct Employee
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public DateTime DateOfEmployment { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string surname, string name, string position, DateTime date, int salary)
+        {
+            Surname = surname;
+            Name = name;
+            Position = position;
+            DateOfEmployment = date;
+            Salary = salary;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Полное имя работника: {0} {1}", Surname, Name);
+            Console.WriteLine("Должность: {0}", Position);
+            Console.WriteLine("Дата приема на работу: {0}", DateOfEmployment);
+            Console.WriteLine("Заработная плата: {0}  тенге", Salary);
+        }
+    }
+}
